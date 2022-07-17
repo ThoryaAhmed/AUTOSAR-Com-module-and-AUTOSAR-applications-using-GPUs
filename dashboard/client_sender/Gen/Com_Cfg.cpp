ebd5521533c7958b0531c30cad399e5dbf232997
nourhan91 <74134249+nourhan91@users.noreply.github.com>
2022-07-17 14:27:54 +0200

    Add files via upload

---
+++ dashboard/client_sender/Gen/Com_Cfg.cpp
@@ -0,0 +1,298 @@
+/*******************************************************************************************************************************
+FileName:                                               Com_Cfg.c
+AUTOSAR Version:                                          4.2.2
+******************************************************************************************************************************/
+/******************************************************************************************************************************
+ **                                                     Includes                                                             **
+ ******************************************************************************************************************************/
+#include "../inc/Com.h"
+#include "Com_Cfg.h"
+#include "../Inc/Com_Types.h"
+#include "../Platform_Types.h"
+#include "../MainWindow.h"
+ /*****************************************************************************************************************************
+  **                                         Post-Build Configuration variables values                                       **
+  *****************************************************************************************************************************/
+
+extern Ui::MainWindowClass* ui_extern;
+
+/* ComSignal Buffers */
+uint8 ComSignal0Buffer[1] = { 0 };
+uint8 ComSignal1Buffer[1] = { 0 };
+uint8 ComSignal2Buffer[1] = { 0 };
+uint8 ComSignal3Buffer[1] = { 0 };
+uint8 ComSignal4Buffer[1] = { 0 };
+uint8 ComSignal5Buffer[1] = { 0 };
+uint8 ComSignal6Buffer[1] = { 0 };
+uint8 ComSignal7Buffer[1] = { 0 }; //8
+uint8 ComSignal8Buffer[1] = { 0 }; //4
+
+// /* ComGroupSignal Buffers */
+// uint8 ComGroupSignal0Buffer[2];
+// uint8 ComGroupSignal1Buffer[2];
+
+/* Com IPdu Buffers */
+uint8 ComIPdu0Buffer[2]; // signals 1 bit
+uint8 ComIPdu1Buffer[2]; // signals of speed and tachometer
+
+void Com_CbkSignal0RxAck(void)
+{
+}
+
+void Com_CbkSignal1RxAck(void)
+{
+    //UARTprintf("Com_CbkSignal1TxAck\n");
+}
+void Com_CbkSignal2RxAck(void)
+{
+    //printf("Com_CbkSignal0TxAck\n");
+}
+
+void Com_CbkSignal3RxAck(void)
+{
+    //UARTprintf("Com_CbkSignal1TxAck\n");
+}
+void Com_CbkSignal4RxAck(void)
+{
+    //printf("Com_CbkSignal0TxAck\n");
+}
+
+void Com_CbkSignal5RxAck(void)
+{
+    //UARTprintf("Com_CbkSignal1TxAck\n");
+}
+void Com_CbkSignal6RxAck(void)
+{
+    //printf("Com_CbkSignal0TxAck\n");
+}
+
+void Com_CbkSignal7RxAck(void)
+{
+    //UARTprintf("Com_CbkSignal1TxAck\n");
+}
+void Com_CbkSignal8RxAck(void)
+{
+    //printf("Com_CbkSignal0TxAck\n");
+}
+
+
+
+
+Com_Type Com =
+{
+    .ComConfig =
+    {
+        .ComIPdu =
+        {
+            {
+               .ComIPduDirection = Receive,
+               .ComIPduHandleId = 0,
+               .ComIPduSignalProcessing = IMMEDIATE,
+               .ComIPduType = NORMAL, // can be omiited
+               .ComIPduGroupRef =
+                {
+                    &Com.ComConfig.ComIPduGroup[0],
+                    NULL
+                },
+               .ComIPduSignalGroupRef =
+                {
+                    &Com.ComConfig.ComSignalGroup[0],
+                    NULL
+                },
+               .ComIPduSignalRef =
+                {
+                    &Com.ComConfig.ComSignal[0],
+                    &Com.ComConfig.ComSignal[1],
+                    &Com.ComConfig.ComSignal[2],
+                    &Com.ComConfig.ComSignal[3],
+                    &Com.ComConfig.ComSignal[4],
+                    &Com.ComConfig.ComSignal[5],
+                    &Com.ComConfig.ComSignal[6],
+                    NULL
+                },
+                .ComTxIPdu =
+                {
+                    .ComMinimumDelayTime = 0.5,
+                    .ComTxIPduClearUpdateBit = Confirmation,
+                    .ComTxIPduUnusedAreasDefault = 255,
+                    .ComTxModeFalse =
+                    {
+                        .ComTxMode =
+                        {
+                            .ComTxModeMode = PERIODIC,
+                            .ComTxModeNumberOfRepetitions = 2,
+                            .ComTxModeRepetitionPeriod = 2,
+                            .ComTxModeTimePeriod = 2
+                        }
+                    }
+                },
+                .ComBufferRef = ComIPdu0Buffer,
+                .ComIPduLength = 2
+            },
+            {
+               .ComIPduDirection = Receive,
+               .ComIPduHandleId = 1,
+               .ComIPduSignalProcessing = IMMEDIATE,
+               .ComIPduType = NORMAL, // can be omiited
+               .ComIPduGroupRef =
+                {
+                    &Com.ComConfig.ComIPduGroup[1],
+                    NULL
+                },
+               .ComIPduSignalGroupRef =
+                {
+                    &Com.ComConfig.ComSignalGroup[1],
+                    NULL
+                },
+               .ComIPduSignalRef =
+                {
+                    &Com.ComConfig.ComSignal[7],
+                    &Com.ComConfig.ComSignal[8],
+                    NULL
+                },
+                .ComTxIPdu =
+                {
+                    .ComMinimumDelayTime = 0.5,
+                    .ComTxIPduClearUpdateBit = Confirmation,
+                    .ComTxIPduUnusedAreasDefault = 255,
+                    .ComTxModeFalse =
+                    {
+                        .ComTxMode =
+                        {
+                            .ComTxModeMode = PERIODIC,
+                            .ComTxModeNumberOfRepetitions = 2,
+                            .ComTxModeRepetitionPeriod = 2,
+                            .ComTxModeTimePeriod = 2
+                        }
+                    }
+                },
+                .ComBufferRef = ComIPdu1Buffer,
+                .ComIPduLength = 2
+            }
+        },
+
+        .ComSignal =
+        {
+            { //
+             .ComBitPosition = 0, // position within ipdu
+             .ComBitSize = 1,	  // size in bits
+             .ComHandleId = 0,
+             .ComNotification = &Com_CbkSignal0RxAck, // only in sender side we may not need it
+             .ComSignalEndianness = LITTLE_ENDIAN, // can be omitted	  // 
+             .ComSignalLength = 1, // size in bytes
+             .ComSignalType = UINT8,// type of signal
+             .ComTransferProperty = TRIGGERED, // 
+             .ComUpdateBitPosition = 7, // can be omitted
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE, //initial value
+             .ComBufferRef = ComSignal0Buffer // value stored of the signal
+            },
+            { //1
+             .ComBitPosition = 1,
+             .ComBitSize = 1,
+             .ComHandleId = 1,
+             .ComNotification = &Com_CbkSignal1RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 1,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = TRIGGERED,
+             .ComUpdateBitPosition = 8,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal1Buffer
+            },
+            { //2
+             .ComBitPosition = 2,
+             .ComBitSize = 1,
+             .ComHandleId = 2,
+             .ComNotification = &Com_CbkSignal2RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 1,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = TRIGGERED,
+             .ComUpdateBitPosition = 9,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal2Buffer
+            },
+            { //3
+             .ComBitPosition = 3,
+             .ComBitSize = 1,
+             .ComHandleId = 3,
+             .ComNotification = &Com_CbkSignal3RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 1,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = TRIGGERED,
+             .ComUpdateBitPosition = 10,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal3Buffer
+            },
+            { //
+             .ComBitPosition = 4,
+             .ComBitSize = 1,
+             .ComHandleId = 4,
+             .ComNotification = &Com_CbkSignal4RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 11,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = TRIGGERED,
+             .ComUpdateBitPosition = 11,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal4Buffer
+            },
+            {
+             .ComBitPosition = 5,
+             .ComBitSize = 1,
+             .ComHandleId = 5,
+             .ComNotification = &Com_CbkSignal5RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 12,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = TRIGGERED,
+             .ComUpdateBitPosition = 12,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal5Buffer
+            },
+            {
+             .ComBitPosition = 6,
+             .ComBitSize = 1,
+             .ComHandleId = 6,
+             .ComNotification = &Com_CbkSignal6RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 13,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = TRIGGERED,
+             .ComUpdateBitPosition = 13,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal6Buffer
+            },
+            {
+             .ComBitPosition = 7,
+             .ComBitSize = 8,
+             .ComHandleId = 7,
+             .ComNotification = &Com_CbkSignal7RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 1,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = PENDING,
+             .ComUpdateBitPosition = 14,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal7Buffer
+            },
+            { //Tachomemter
+
+             .ComBitPosition = 8,
+             .ComBitSize = 4,
+             .ComHandleId = 8,
+             .ComNotification = &Com_CbkSignal8RxAck,
+             .ComSignalEndianness = LITTLE_ENDIAN,
+             .ComSignalLength = 1,
+             .ComSignalType = UINT8,
+             .ComTransferProperty = PENDING,
+             .ComUpdateBitPosition = 15,
+             .ComSignalInitValue = COM_SIGNAL_INIT_VALUE,
+             .ComBufferRef = ComSignal8Buffer
+            }
+        }
+    }
+
+
+};
---
+++ dashboard/client_sender/Gen/Com_Cfg.h
@@ -0,0 +1,108 @@
+#pragma once
+/*******************************************************************************************************************************
+FileName:                                               Com_Cfg.h
+AUTOSAR Version:                                          4.2.2
+******************************************************************************************************************************/
+#ifndef COM_CFG_H
+#define COM_CFG_H
+
+
+
+/*******************************************************************************************************************************
+**                                                                        Defines                                                                                **
+********************************************************************************************************************************/
+#define   COM_TX_IPDU_UNUSED_AREAS_DEFAULT   (uint8)0x00
+#define   COM_SIGNAL_INIT_VALUE (uint8)0xFF
+#define ComMaxSignalGroupCnt   (uint8)2
+#define ComMaxGroupSignalCnt   (uint8)2
+#define ComMaxSignalCnt   (uint8)9
+#define ComMaxIPduGroupCnt (uint8)2
+
+
+
+/**************************************************************************************************
+**
+Name:                                   ComConfigurationUseDet
+Type:                                   EcucBooleanParamDef
+Description:            The error hook shall contain code to call the Det.
+                        If this parameter is configured COM_DEV_ERROR_DETECT shall be set
+                        to ON as output of the configuration tool. (as input for the source code)                                           **
+**************************************************************************************************/
+#define ComConfigurationUseDet false
+
+
+/**************************************************************************************************
+**
+Name:                                   ComEnableMDTForCyclicTransmission
+Type:                                   EcucBooleanParamDef
+Description:            Enables globally for the whole Com module the minimum delay time monitoring
+                        for cyclic and repeated transmissions (ComTxModeMode=PERIODIC or ComTxModeMode=MIXED
+                        for the cyclic transmissions,
+                        ComTxModeNumberOfRepetitions > 0 for repeated transmissions).                                         **
+**************************************************************************************************/
+#define ComEnableMDTForCyclicTransmission false
+
+
+/**************************************************************************************************
+**
+Name:                                   ComRetryFailedTransmitRequests
+Type:                                   EcucBooleanParamDef
+Description:                If this Parameter is set to true, retry of failed transmission requests
+                            is enabled. If this Parameter is not present, the default value is assumed                              **
+**************************************************************************************************/
+#define ComRetryFailedTransmitRequests  false
+
+
+
+/**************************************************************************************************
+Name:                                ComSupportedIPduGroups
+Type:                                EcucIntegerParamDef
+Description:                Defines the maximum number of supported I-PDU groups.
+Range:                               0 ---> 65535
+**************************************************************************************************/
+#define ComSupportedIPduGroups  (uint16)    2
+
+
+/**************************************************************************************************
+**
+Name:                                   ComRxTimeBase
+Type:                                   EcucFloatParamDef
+Description:                The period between successive calls to Com_MainFunctionRx in seconds.
+                            This parameter may be used by the COM generator to transform the values
+                            of the reception related timing configuration parameters of the COM
+                            module to internal implementation specific counter or tick values. The
+                            COM module's internal timing handling is implementation specific.
+                            The COM module (generator) may rely on the fact that
+                            Com_MainFunctionRx is scheduled according to the value configuredhere
+Range:                                  0 ---> 3600                                         **
+**************************************************************************************************/
+#define ComRxTimeBase   (float32)    0.1
+
+
+/**************************************************************************************************
+**
+Name:                                   ComTxTimeBase
+Type:                                   EcucFloatParamDef
+Description:                The period between successive calls to Com_MainFunctionTx in seconds.
+                            This parameter may be used by the COM generator to transform
+                            the values of the transmission related timing configuration parameters of the COM
+                            module to internal implementation specific counter or tick values. The
+                            COM module's internal timing handling is implementation specific.
+                            The COM module (generator) may rely on the fact that
+                            Com_MainFunctionTx is scheduled according to the value configured here
+Range:                                  0 ---> 3600
+**************************************************************************************************/
+#define ComTxTimeBase   (float32)    0.5
+
+
+/**************************************************************************************************
+Name:                                ComMaxIPduCnt
+Type:                                EcucIntegerParamDef
+Description:                Defines the maximum number of supported I-PDU groups.
+Range:                               0 ---> 18446744073709551615
+**************************************************************************************************/
+#define ComMaxIPduCnt  (uint64)    2
+
+
+
+#endif /*COM_CFG_H*/
