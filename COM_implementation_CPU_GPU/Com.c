67ecdc76a1e29ac1ab35b5c1f091b3c132898da3
MostafaMohsen99 <66281065+mostafamohsen99@users.noreply.github.com>
2022-07-17 14:07:28 +0200

    Create Com.c

--- COM_implementation_CPU_GPU/Com.c
+++ COM_implementation_CPU_GPU/Com.c
@@ -1,68 +1,31 @@
-
 /*********************************************************************************************************************************
- Service name:               Com_ReceiveSignalGroup
- Service ID:                    0x0e
- Parameters (in):               SignalGroupId -->  Id of signal group to be sent.
+ Service name:               Com_ReceiveShadowSignal
+ Service ID:                    0x0f
+ Parameters (in):           SignalId--> Id of group signal to be received.
+                            SignalDataPtr --> Reference to the group signal data in which to store the received data.
  Parameters (inout):            None
  Parameters (out):              None
- Return value:                  uint8 --->
-                                 ->E_OK: service has been accepted
-                                 ->COM_SERVICE_NOT_AVAILABLE: corresponding I-PDU group
-                                     was stopped (or service failed due to development error)
-                                 ->COM_BUSY: in case the TP-Buffer is locked for large data types
-                                     handling
- Description:        The service Com_ReceiveSignalGroup copies the received signal group from the
-                     I-PDU to the shadow buffer.
+ Return value:                  None
+ Description:        The service Com_ReceiveShadowSignal updates the group signal which is referenced by SignalDataPtr with the data in the shadow buffer.
  *******************************************************************************************************************************/
-uint8 Com_ReceiveSignalGroup(Com_SignalGroupIdType SignalGroupId)
+void Com_ReceiveShadowSignal(Com_SignalIdType SignalId, void* SignalDataPtr)
 {
-    uint8 ComIPduIndex, ComSignalGroupIndex, ComGroupSignalIndex, BitIndex;
-    Com_GroupSignalType* ComGroupSignalLocal;
-    Com_IPduType* ComIPduLocal;
+    uint8 ComGroupSignalIndex;
 
-    /*[SWS_Com_00638] âŒˆThe service Com_ReceiveSignalGroup shall copy the received
-    signal group from the I-PDU to the shadow buffer.âŒ‹ (SRS_Com_02041)*/
-    if(SignalGroupId < ComMaxSignalGroupCnt)
+    /* Check that the group signal ID is a valid ID*/
+    if(SignalId < ComMaxGroupSignalCnt)
     {
-        /*After this call, the group signals could be copied from the shadow buffer to the RTE
-        by calling Com_ReceiveSignal.*/
-        /* Find the IPdu which contains this signal */
-
-        ComGroupSignalLocal = &Com.ComConfig.ComGroupSignal[SignalGroupId];
-
-
-        for(ComIPduIndex = 0; ComIPduIndex < ComMaxIPduCnt; ComIPduIndex++)
+        /*Find GroupSignal with such ID*/
+        for(ComGroupSignalIndex = 0; ComGroupSignalIndex < ComMaxGroupSignalCnt; ComGroupSignalIndex++)
         {
-            for(ComSignalGroupIndex = 0; Com.ComConfig.ComIPdu[ComIPduIndex].ComIPduSignalGroupRef[ComSignalGroupIndex] != NULL; ComSignalGroupIndex++)
+            if(Com.ComConfig.ComGroupSignal[ComGroupSignalIndex].ComHandleId == SignalId)
             {
-                if(Com.ComConfig.ComIPdu[ComIPduIndex].ComIPduSignalGroupRef[ComSignalGroupIndex]->ComHandleId == SignalGroupId)
-                {
-                    /* Get IPdu */
-                    ComIPduLocal = &Com.ComConfig.ComIPdu[ComIPduIndex];
+                memcpy(SignalDataPtr, Com.ComConfig.ComGroupSignal[ComGroupSignalIndex].ComBufferRef, Com.ComConfig.ComGroupSignal[ComGroupSignalIndex].ComSignalLength);
+                return;
+            }
+            else
+            {
 
-                    for(ComGroupSignalIndex = 0; ComIPduLocal->ComIPduSignalGroupRef[ComSignalGroupIndex]->ComGroupSignalRef[ComGroupSignalIndex] != NULL; ComGroupSignalIndex++)
-                    {
-                        /*Get Group Signal*/
-                        ComGroupSignalLocal = ComIPduLocal->ComIPduSignalGroupRef[ComSignalGroupIndex]->ComGroupSignalRef[ComGroupSignalIndex];
-
-                        /* Write data from IPdu to GroupSignal buffer to IPdu*/
-                        for(BitIndex = ComGroupSignalLocal->ComBitPosition; BitIndex < ComGroupSignalLocal->ComBitPosition + ComGroupSignalLocal->ComBitSize; BitIndex++)
-                        {
-                            if((ComIPduLocal->ComBufferRef[BitIndex / 8] >> (BitIndex % 8)) & 1)
-                            {
-                               ComGroupSignalLocal->ComBufferRef[(BitIndex - ComGroupSignalLocal->ComBitPosition) / 8] |= 1 << ((BitIndex - ComGroupSignalLocal->ComBitPosition) % 8);
-                            }
-                           else
-                            {
-                               ComGroupSignalLocal->ComBufferRef[(BitIndex - ComGroupSignalLocal->ComBitPosition) / 8] &= ~(1 << ((BitIndex - ComGroupSignalLocal->ComBitPosition) % 8));
-                            }
-                        }
-                        return E_OK;
-                    }
-                }
-                else
-                {
-                }
             }
         }
     }
@@ -70,5 +33,5 @@ uint8 Com_ReceiveSignalGroup(Com_SignalGroupIdType SignalGroupId)
     {
 
     }
-    return COM_SERVICE_NOT_AVAILABLE;
 }
+
